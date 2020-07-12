a0269745977b03745d192c0dc4e35ee12b70bcd6
ColeRutledge <ColeRutledge@gmail.com>
2020-07-11 23:46:13 -0400

    add animations

--- src/components/SortNode.js
+++ src/components/SortNode.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-// import { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import DataContext from '../contexts/DataContext'
 import { Node } from '../styles'
@@ -7,17 +7,29 @@ import { Node } from '../styles'
 const SortNode = ({ value }) => {
   const { dataSize } = useContext(DataContext)
 
-  // const tween = {
-  //   type: "tween",
-  //   damping: 500,
-  //   stiffness: 500,
-  //   duration: 0.1,
-  // }
+  const tween = {
+    type: "tween",
+    damping: 500,
+    stiffness: 500,
+    duration: 0.1,
+  }
+
+  const divStyles = {
+    backgroundColor: '#02203c',
+    height: `${(value + 1) * (600 / dataSize)}px`,
+    width: '100%',
+    margin: '1px',
+    borderRadius: '4px',
+  }
+
+
+//   &:hover { background-color: #032A4F !important;
 
   return (
     <>
       {/* <motion.div layoutTransition={tween} className='bar' style={divStyles}></motion.div> */}
-      <Node className='bar' style={{ height: `${(value + 1) * (600 / dataSize)}px` }}></Node>
+      <motion.div layoutTransition={tween} className='bar' style={divStyles}></motion.div>
+      {/* <Node className='bar' style={{ height: `${(value + 1) * (600 / dataSize)}px` }}></Node> */}
     </>
   )
 }
